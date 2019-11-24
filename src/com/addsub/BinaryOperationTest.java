6bff116a24032bd48b693a5decc46c7a09fea7e3
humanfy <1727796150@qq.com>
2019-11-24 14:19:08 +0800

    解决部分汉字乱码问题

--- src/com/addsub/BinaryOperationTest.java
+++ src/com/addsub/BinaryOperationTest.java
@@ -4,12 +4,12 @@ public class BinaryOperationTest {
 
 	public static void main(String[] args) {
 		BinaryOperation_1_1 aop;
-		System.out.println("加法算式为:");
+		System.out.println("加法习题为：");
 		for (int i = 0; i < 10; i++) {
 			aop=new AddOperation();
 			System.out.println(aop);
 		}
-		System.out.println("减法算式为:");
+		System.out.println("减法习题为：");
 		for (int i = 0; i < 10; i++) {
 			aop=new SubOperation();
 			System.out.println(aop);
